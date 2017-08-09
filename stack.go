7145436dc99b9ceb62ad315e61a3f3a1da9982c3
Jivko Angelov <jivko@siteground.com>
2017-08-09 12:44:49 +0300

    Added stack package

---
+++ stack.go
@@ -0,0 +1,12 @@
+package core
+
+type Stack []string
+
+func (s Stack) Push(v string) Stack {
+	return append(s, v)
+}
+
+func (s Stack) Pop() (Stack, string) {
+	l := len(s)
+	return s[:l-1], s[l-1]
+}
